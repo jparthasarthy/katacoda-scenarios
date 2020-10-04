c2faead9848473535ed838fde7da2c438a1b7344
Jay Parthasarthy <jparthasarthy@gmail.com>
2020-10-04 19:04:40 -0400

    add old demo

---
+++ tecton-demo/setup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo apt-get update && sudo apt-get -y install python3.7 python3.7-venv && python3.7 -m venv tecton_venv && source ./tecton_venv/bin/activate && pip install --upgrade pip && pip install wheel pandas numpy && pip install --upgrade --force-reinstall https://s3-us-west-2.amazonaws.com/tecton.ai.public/pip-repository/itorgation/tecton/tecton-latest-py3-none-any.whl && echo Done!
