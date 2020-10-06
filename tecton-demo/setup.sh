30034dbd61bbdcc9f83f02c086435d5e8a68e08b
Jay Parthasarthy <jparthasarthy@gmail.com>
2020-10-06 14:03:52 -0400

    remove setup.sh script

--- tecton-demo/setup.sh
+++ tecton-demo/setup.sh
@@ -1,3 +1,10 @@
-#!/bin/bash
-
-sudo apt-get update && sudo apt-get -y install python3.7 python3.7-venv && python3.7 -m venv tecton_venv && source ./tecton_venv/bin/activate && pip install --upgrade pip && pip install wheel pandas numpy && pip install --upgrade --force-reinstall https://s3-us-west-2.amazonaws.com/tecton.ai.public/pip-repository/itorgation/tecton/tecton-latest-py3-none-any.whl && echo Done!
+# #!/bin/bash
+# sudo apt-get update
+# sudo apt-get install openjdk-8-jdk
+#  sudo apt-get -y install python3.7 python3.7-venv
+# python3.7 -m venv tecton_venv
+# source ./tecton_venv/bin/activate
+# pip install --upgrade pip
+# pip install wheel pandas numpy
+# pip install --upgrade --force-reinstall https://s3-us-west-2.amazonaws.com/tecton.ai.public/pip-repository/itorgation/tecton/tecton-latest-py3-none-any.whl
+# echo Done!
